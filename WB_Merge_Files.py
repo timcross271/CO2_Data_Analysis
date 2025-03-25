3b9c20d430eb1ee7abe6280ceed80a0ac816cb6c
timcross271 <timcross271@gmail.com>
2025-03-25 16:48:55 +0000

    Deleted file

---
+++ WB_Merge_Files.py
@@ -0,0 +1,15 @@
+# Merge two seperate data files to one file for analysis.
+
+# Import libraries
+import pandas as pd
+import numpy as np
+
+# Load two seperate csv files containing World Bank data
+cd1=pd.read_csv('Combined_data_1.csv')
+cd2=pd.read_csv('Combined_data_2.csv')
+
+# Merge csv files to new panda dataframe
+merged_cd=pd.merge(cd2,cd1,on=('Series Name','Series Code','Country Name','Country Code'))
+print(merged_cd.dtypes)
+
+merged_cd.to_csv('Merged_Combined_data.csv')
